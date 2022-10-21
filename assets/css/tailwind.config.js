828f79e9179b30e6efa606ca5996a14b925df517
Kien Nguyen <kiennt2609@gmail.com>
2022-10-21 09:33:31 +0700

    Tailwind + PostCSS

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./themes/**/layouts/**/*.html",
+    "./content/**/layouts/**/*.html",
+    "./layouts/**/*.html",
+    "./content/**/*.html"
+  ],
+  theme: {
+    extend: {
+        spacing: {
+				'page-top': 'var(--page-top)',
+				'page-gutter': 'var(--page-gutter)',
+				'header-height': 'var(--header-height)',
+				'footer-height': 'var(--footer-height)',
+			},
+			maxWidth: {
+				content: 'var(--content-width)',
+			},
+			minHeight: {
+				content: 'var(--content-height)',
+			},
+			fontSize: {
+				md: '15px',
+			},
+			colors: {
+				'b-low-a': 'var(--b-low-a)',
+				'b-low': 'var(--b-low)',
+				'b-med': 'var(--b-med)',
+				'f-med': 'var(--f-med)',
+				'f-high': 'var(--f-high)',
+			},
+			borderColor: (theme) => ({
+				DEFAULT: theme('colors.b-med', 'currentColor'),
+			}),
+		},
+  },
+  plugins: []
+}
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["content/**/*.md", "layouts/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
