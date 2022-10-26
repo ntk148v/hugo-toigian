0340c7b06390caa9733e456307e5d06572f411d9
Kien Nguyen <kiennt2609@gmail.com>
2022-10-26 10:34:03 +0700

    Update dark mode toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,30 @@
 window.addEventListener("DOMContentLoaded", function () {
-    const dark_mode_btn = document.getElementById("dark_mode_btn");
-    const light_mode_btn = document.getElementById("light_mode_btn");
+  const dark_mode_btn = document.getElementById("dark_mode_btn");
+  const light_mode_btn = document.getElementById("light_mode_btn");
 
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        light_mode_btn.classList.remove('hidden');
-    } else {
-        dark_mode_btn.classList.remove('hidden');
-    }
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+    light_mode_btn.classList.remove("hidden");
+  } else {
+    document.documentElement.classList.remove("dark");
+    dark_mode_btn.classList.remove("hidden");
+  }
 
-    dark_mode_btn.addEventListener('click', function () {
-        document.documentElement.setAttribute("site-theme", "dark");
-        localStorage.theme = 'dark';
-        dark_mode_btn.classList.add('hidden');
-        light_mode_btn.classList.remove('hidden');
-    });
+  dark_mode_btn.addEventListener("click", function () {
+    localStorage.theme = "dark";
+    document.documentElement.classList.add("dark");
+    dark_mode_btn.classList.add("hidden");
+    light_mode_btn.classList.remove("hidden");
+  });
 
-    light_mode_btn.addEventListener('click', function () {
-        document.documentElement.setAttribute("site-theme", "light");
-        localStorage.theme = 'light';
-        light_mode_btn.classList.add('hidden');
-        dark_mode_btn.classList.remove('hidden');
-    });
+  light_mode_btn.addEventListener("click", function () {
+    localStorage.theme = "light";
+    document.documentElement.classList.remove("dark");
+    light_mode_btn.classList.add("hidden");
+    dark_mode_btn.classList.remove("hidden");
+  });
 });
