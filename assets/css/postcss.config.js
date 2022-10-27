090d4c9defb6005a564db7352ba049cfbaaa8703
Kien Nguyen <kiennt2609@gmail.com>
2022-10-27 18:18:02 +0700

    Update workflow

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,25 +1,10 @@
 const themeDir = __dirname + '/../../';
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
-    content: [
-        './hugo_stats.json',
-        themeDir + '/hugo_stats.json',
-        'exampleSite/hugo_stats.json',
-    ],
-    safelist : [ /type/ ],
-    defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-    }
-})
-
 module.exports = {
   plugins: {
     'postcss-import': { path: [themeDir] },
     'tailwindcss/nesting': {},
     tailwindcss: { config:  themeDir + 'assets/css/tailwind.config.js' },
     autoprefixer: { path: [themeDir] },
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
   }
 }
